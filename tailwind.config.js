5a52e0d5bf0825b2767d747b14c8faf6ec45e5f3
iamjohnnysky <vanpasie20@gmail.com>
2025-04-09 12:36:44 -0700

    tailwind is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media', // or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
